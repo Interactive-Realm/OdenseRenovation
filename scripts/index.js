20b6a8171d6b626853b04ba811b2a411b09413c5
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2023-06-30 15:02:49 +0200

    lottie anims working

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+console.log("Hello World");
+
+
+
+let player = document.getElementById("lottiePlayer_RandomAnim");
+
+player.addEventListener("ready", () => {
+  LottieInteractivity.create({
+        mode:"scroll",
+        player: "#lottiePlayer_RandomAnim",
+        actions: [
+        {
+            visibility:[0.15, 1.0],
+            type: "seek",
+            frames: [0, 70],
+        },
+        ]
+      });
+});
