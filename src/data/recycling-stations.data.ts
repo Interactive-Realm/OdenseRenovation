9891b17f8a235bd1b3538118858374ee79c25dbe
Karsten Pedersen <karstenfp.all@gmail.com>
2023-07-06 19:18:19 +0200

    Worked on recycling station section

---
+++ src/data/recycling-stations.data.ts
@@ -0,0 +1,36 @@
+import type { RecyclingStation } from "../logic/types";
+
+export const recyclingStations: RecyclingStation[] = [
+  {
+    title: "Bullerupvej 84 B, Seden",
+    location: "5240 Odense NØ",
+  },
+  {
+    title: "Holkebjergvej 132, Højme",
+    location: "5250 Odense SV",
+  },
+  {
+    title: "Egestubben 2 F, Næsby",
+    location: "5270 Odense N",
+  },
+  {
+    title: "Energivej 10, Lindved",
+    location: "5260 Odense S",
+  },
+  {
+    title: "Havnegade 102, Odense C",
+    location: "5000 Odense C",
+  },
+  {
+    title: "Niels Bohrs Alle 148 B ved Bilka",
+    location: "5230 Odense M",
+  },
+  {
+    title: "Rugårdsvej 275, Villestofte",
+    location: "5210 Odense NV",
+  },
+  {
+    title: "Snapindvej 21, Bolbro",
+    location: "5200 Odense V",
+  },
+];
--- src/logic/types.ts
+++ src/logic/types.ts
@@ -19,3 +19,8 @@ export type Breadcrumb = {
   title: string;
   link: string;
 };
+
+export type RecyclingStation = {
+  title: string;
+  location: string;
+};
